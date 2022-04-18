e6301ac923faabe1f16bfdabe319574ee738c186
Gary Kaufman <garykkaufman@gmail.com>
2022-04-17 23:02:47 -0500

    Added rollDescription and cleaned up HomeView, App

--- src/components/DieRoll.vue
+++ src/components/DieRoll.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
       <h1 @click="roll" id="die">{{ rollResult }}</h1>
+      <caption id="rollDescription">{{ rollDescription }}</caption>
   </div>
 </template>
 
@@ -9,33 +10,41 @@ import { Vue } from "vue-class-component";
 
 export default class DieRoll extends Vue {
     rollResult = "";
+    rollDescription = "";
 
     roll(): void{
         document.querySelector("#die")?.classList.add("roll");
+        this.rollDescription = "";
         setTimeout(() => {
         let result = Math.floor(Math.random() * 6) + 1;
         if (result == 1) {
             this.rollResult = "1"
+            this.rollDescription = "Move 1 space if you get all correct answers this round!"
             document.querySelector("#die")?.classList.remove("special");
             }
         if (result == 2) {
             this.rollResult = "2"
+            this.rollDescription = "Move 2 space if you get all correct answers this round!"
             document.querySelector("#die")?.classList.remove("special");
             }
         if (result == 3) {
             this.rollResult = "3"
+            this.rollDescription = "Move 3 space if you get all correct answers this round!"
             document.querySelector("#die")?.classList.remove("special");
             }
         if (result == 4) {
             this.rollResult = "4"
+            this.rollDescription = "Move 4 space if you get all correct answers this round!"
             document.querySelector("#die")?.classList.remove("special");
             }
         if (result == 5) {
             this.rollResult = "+1";
+            this.rollDescription = "Bonus Points are DOUBLED! Move 1 space if you get all correct answers this round!"
             document.querySelector("#die")?.classList.add("special");
         }
         if (result == 6) {
             this.rollResult = "¡2!"
+            this.rollDescription = "Move 2 space if you get all INCORRECT answers this round!"
             document.querySelector("#die")?.classList.add("special")
         }
         document.querySelector("#die")?.classList.remove("roll");
@@ -58,6 +67,16 @@ export default class DieRoll extends Vue {
     width: 50px;
     border: 2px solid black;
     border-radius: 3px;
+}
+
+caption {
+    display: block;
+    margin-top: 10px;
+}
+
+.hide {
+    display: none;
+    margin-top: 10px;
 }
 
 .special {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
+    <h1>Welcome to the Half Truth Game Interface!</h1>
     <DieRoll/>
-<!--     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import DieRoll from '@/components/DieRoll.vue'
 
 @Options({
   components: {
-    HelloWorld,
     DieRoll
   },
 })
