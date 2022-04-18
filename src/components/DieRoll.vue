c29ee164b8f24b5d5fec1f513e105682e1071021
Gary Kaufman <garykkaufman@gmail.com>
2022-04-17 21:21:08 -0500

    Created basic DieRoll and adjusted HomeView

---
+++ src/components/DieRoll.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <button @click="roll">Roll Die</button>
+      <p>rolled die value = {{ rollResult }}</p>
+  </div>
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+
+export default class DieRoll extends Vue {
+    rollResult = 0;
+
+    roll() {
+        this.rollResult = Math.floor(Math.random() * 6) + 1;
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <DieRoll/>
+<!--     <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import DieRoll from '@/components/DieRoll.vue'
 
 @Options({
   components: {
     HelloWorld,
+    DieRoll
   },
 })
 export default class HomeView extends Vue {}
